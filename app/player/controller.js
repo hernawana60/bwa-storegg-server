f51c949e0c0e53ec64a54e8884acb987b8263b2d
hernawana60 <hernawana60@gmail.com>
2021-10-16 00:24:39 +0700

    controller player

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,13 +1,14 @@
-const Player = require ('./model')
-const Voucher = require ('../voucher/model')
-const Category = require ('../category/model')
-const Payment = require ('../payment/model')
-const Bank = require ('../bank/model')
-const Nominal = require ('../nominal/model')
-const Transaction = require ('../transaction/model')
+const Player = require('./model')
+const Voucher = require('../voucher/model')
+const Category = require('../category/model')
+const Bank = require('../bank/model')
+const Payment = require('../payment/model')
+const Nominal = require('../nominal/model')
+const Transaction = require('../transaction/model')
 const path = require('path')
 const fs = require('fs')
 const config = require('../../config')
+
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -55,60 +56,63 @@ module.exports = {
     }
   },
 
-  checkout : async (req, res)=>{
+  checkout: async (req, res) => {
     try {
-      const {accountUser, name, nominal, voucher, payment, bank} = req.body
+      const { accountUser, name, nominal, voucher, payment, bank } = req.body
 
-      const res_voucher = await Voucher.findOne({_id : voucher})
-      .select('name category _id thumbnail user')
-      .populate('category')
-      .populate('user')
+      const res_voucher = await Voucher.findOne({ _id: voucher })
+        .select('name caegory _id thumbnail user')
+        .populate('category')
+        .populate('user')
 
-      if(!res_voucher) return res.status(404).json({ message: 'voucher game tidak ditemukan.' })
+      if (!res_voucher) return res.status(404).json({ message: 'voucher game tidak ditemukan.' })
 
-      const res_nominal = await Nominal.findOne({_id : nominal})
+      const res_nominal = await Nominal.findOne({ _id: nominal })
 
-      if(!res_nominal) return res.status(404).json({ message: 'nominal tidak ditemukan.' })
-      
-      const res_payment = await Payment.findOne({_id : payment})
+      if (!res_nominal) return res.status(404).json({ message: 'nominal tidak ditemukan.' })
 
-      if(!res_payment) return res.status(404).json({ message: 'payment tidak ditemukan.' })
+      const res_payment = await Payment.findOne({ _id: payment })
 
-      const res_bank = await Bank.findOne({_id : bank})
+      if (!res_payment) return res.status(404).json({ message: 'payment tidak ditemukan.' })
 
-      if(!res_bank) return res.status(404).json({ message: 'bank tidak ditemukan.' })
+      const res_bank = await Bank.findOne({ _id: bank })
+
+      if (!res_bank) return res.status(404).json({ message: 'payment tidak ditemukan.' })
 
       let tax = (10 / 100) * res_nominal._doc.price;
       let value = res_nominal._doc.price - tax;
 
+
+      console.log("res_payment >>")
+      console.log(res_payment._doc)
       const payload = {
-        historyVoucherTopup : {
-          gameName : res_voucher._doc.name,
-          category : res_voucher._doc.category ? res_voucher._doc.category.name : '',
-          thumbnail : res_voucher._doc.thumbnail,
-          coinName : res_nominal._doc.coinName,
-          coinQuantity : res_nominal._doc.coinQuantity,
-          price : res_nominal._doc.price,
+        historyVoucherTopup: {
+          gameName: res_voucher._doc.name,
+          category: res_voucher._doc.category ? res_voucher._doc.category.name : '',
+          thumbnail: res_voucher._doc.thumbnail,
+          coinName: res_nominal._doc.coinName,
+          coinQuantity: res_nominal._doc.coinQuantity,
+          price: res_nominal._doc.price,
         },
-        historyPayment : {
-          name : res_bank._doc.name,
-          type : res_payment._doc.type,
-          bankName : res_bank._doc.bankName,
-          noRekening : res_bank._doc.noRekening,
-        },
-
-        name : name,
-        accountUser : accountUser,
-        tax : tax,
-        value : value,
-        player : req.player._id,
-        historyUser : {
-          name : res_voucher._doc.user?.name,
-          phoneNumber : res_voucher._doc.user?.phoneNumber
+        historyPayment: {
+          name: res_bank._doc.name,
+          type: res_payment._doc.type,
+          bankName: res_bank._doc.bankName,
+          noRekening: res_bank._doc.noRekening,
         },
 
-        category : res_voucher._doc.category?._id,
-        user : res_voucher._doc.user?._id
+        name: name,
+        accountUser: accountUser,
+        tax: tax,
+        value: value,
+        player: req.player._id,
+        historyUser: {
+          name: res_voucher._doc.user?.name,
+          phoneNumber: res_voucher._doc.user?.phoneNumber
+        },
+
+        category: res_voucher._doc.category?._id,
+        user: res_voucher._doc.user?._id
 
       }
 
@@ -125,7 +129,7 @@ module.exports = {
     }
   },
 
-   history: async (req, res) => {
+  history: async (req, res) => {
     try {
       const { status = '' } = req.query;
 
@@ -148,18 +152,18 @@ module.exports = {
       const history = await Transaction.find(criteria)
 
       let total = await Transaction.aggregate([
-        {$match : criteria},
+        { $match: criteria },
         {
-          $group :{
+          $group: {
             _id: null,
-            value : {$sum: "$value"}
+            value: { $sum: "$value" }
           }
         }
       ])
 
       res.status(200).json({
         data: history,
-        total : total.length ? total[0].value : 0
+        total: total.length ? total[0].value : 0
       })
 
     } catch (err) {
@@ -167,7 +171,7 @@ module.exports = {
     }
   },
 
-   historyDetail: async (req, res) => {
+  historyDetail: async (req, res) => {
     try {
       const { id } = req.params
 
@@ -182,14 +186,14 @@ module.exports = {
     }
   },
 
-  dashboard: async (req, res)=>{
+  dashboard: async (req, res) => {
     try {
       const count = await Transaction.aggregate([
-        { $match : {player : req.player._id}},
+        { $match: { player: req.player._id } },
         {
-          $group : {
-            _id : '$category',
-            value :{ $sum: '$value' }
+          $group: {
+            _id: '$category',
+            valeu: { $sum: '$value' }
           }
         }
       ])
@@ -197,24 +201,24 @@ module.exports = {
       const category = await Category.find({})
 
       category.forEach(element => {
-        count.forEach(data=>{
-          if(data._id.toString() === element._id.toString()){
+        count.forEach(data => {
+          if (data._id.toString() === element._id.toString()) {
             data.name = element.name
           }
         })
       });
 
       const history = await Transaction.find({ player: req.player._id })
-      .populate('category')
-      .sort({'updateAt': -1})
+        .populate('category')
+        .sort({ 'updatedAt': -1 })
 
-      res.status(200).json({ data: history, count })
+      res.status(200).json({ data: history, count: count })
     } catch (err) {
       res.status(500).json({ message: err.message || `Internal server error` })
     }
   },
 
-  profile: async (req, res)=>{
+  profile: async (req, res) => {
     try {
 
       const player = {
@@ -227,66 +231,68 @@ module.exports = {
       }
 
       res.status(200).json({ data: player })
-      
+
     } catch (err) {
       res.status(500).json({ message: err.message || `Internal server error` })
     }
   },
 
-  editProfile: async (req, res, next)=>{
+  editProfile: async (req, res, next) => {
     try {
       const { name = "", phoneNumber = "" } = req.body
 
       const payload = {}
 
-      if(name.length) payload.name = name
-      if(phoneNumber.length) payload.phoneNumber = phoneNumber
+      if (name.length) payload.name = name
+      if (phoneNumber.length) payload.phoneNumber = phoneNumber
 
-      if(req.file){
+      if (req.file) {
 
-        let tmp_path= req.file.path;
+        let tmp_path = req.file.path;
         let originaExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
-        let filename = req.file.filename+'.' +originaExt;
+        let filename = req.file.filename + '.' + originaExt;
         let target_path = path.resolve(config.rootPath, `public/uploads/${filename}`)
 
         const src = fs.createReadStream(tmp_path)
         const dest = fs.createWriteStream(target_path)
+
         src.pipe(dest)
-        src.on('end', async ()=>{
-                let player = await Player.findOne({_id : req.player._id})
 
-                let currentImage = `${config.rootPath}/public/uploads/${player.avatar}`;
-                if(fs.existsSync(currentImage)){
-                    fs.unlinkSync(currentImage)
-                }
+        src.on('end', async () => {
+          let player = await Player.findOne({ _id: req.player._id })
 
-                player = await Player.findOneAndUpdate({
-                    _id : req.player._id
-                },{
-                    ...payload,
-                    avatar: filename
-                }, {new:true, runValidators:true})
+          let currentImage = `${config.rootPath}/public/uploads/${player.avatar}`;
+          if (fs.existsSync(currentImage)) {
+            fs.unlinkSync(currentImage)
+          }
 
-                console.log(player)
+          player = await Player.findOneAndUpdate({
+            _id: req.player._id
+          }, {
+            ...payload,
+            avatar: filename
+          }, { new: true, runValidators: true })
 
-                res.status(201).json({
-                  data: {
-                    id: player.id,
-                    name: player.name,
-                    phoneNumber: player.phoneNumber,
-                    avatar: player.avatar,
-                  }
-                })
+          console.log(player)
+
+          res.status(201).json({
+            data: {
+              id: player.id,
+              name: player.name,
+              phoneNumber: player.phoneNumber,
+              avatar: player.avatar,
+            }
           })
-        
-        src.on('err', async()=>{
-          next()
+        })
+
+        src.on('err', async () => {
+          next(err)
         })
 
       } else {
         const player = await Player.findOneAndUpdate({
           _id: req.player._id
-        }, payload, {new:true, runValidators:true})
+        }, payload, { new: true, runValidators: true })
 
         res.status(201).json({
           data: {
@@ -299,7 +305,7 @@ module.exports = {
       }
 
     } catch (err) {
-      if(err && err.name === "ValidationError"){
+      if (err && err.name === "ValidationError") {
         res.status(422).json({
           error: 1,
           message: err.message,
@@ -308,5 +314,6 @@ module.exports = {
       }
     }
   }
+
 
 }
